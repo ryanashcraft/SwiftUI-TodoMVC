560df166c33c1efefdc3a663a7c922d182bdcb25
Ryan Ashcraft <ryan@ryanashcraft.me>
2019-07-09 11:37:02 -0700

    Initial Commit

---
+++ TodoMVC/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TodoMVC
+//
+//  Created by Ryan Ashcraft on 7/9/19.
+//  Copyright © 2019 Ryan Ashcraft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
